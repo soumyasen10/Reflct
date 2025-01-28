809e2e1fe5f10ddc1ca5042700a198b24c4ada15
soumyasen10 <soumyasenofficial@gmail.com>
2025-01-28 20:26:56 +0530

    "project init and auth done"

---
+++ components/Header.jsx
@@ -0,0 +1,68 @@
+"use client"
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { ChartColumn, FolderOpenIcon, PenBox } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header className="container mx-auto ">
+      <nav className="py-6 px-4 flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src={"/logo.png"}
+            alt="logo"
+            width={200}
+            height={60}
+            className="h-10 w-auto object-contain"
+          />
+        </Link>
+
+        <div className="flex items-center gap-4">
+         
+          <SignedIn>
+          <Link href='/dashboard#collections'>
+          <Button variant="outline" className="flex items-center gap-2">
+            <FolderOpenIcon size={18}/>
+           <span className="hidden md:inline"> Collections</span>
+          </Button>
+          </Link>
+          </SignedIn>
+
+          <Link href='/journal/write'>
+          <Button variant="orange" className="flex items-center gap-2">
+            <PenBox size={18}/>
+           <span className="hidden md:inline"> Write New</span>
+          </Button>
+          </Link>
+
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/dashboard">
+                <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+            <UserButton appearance={{
+                elements:{
+                    avatarBox:"w-10 h-10"
+                }
+            }}>
+                <UserButton.MenuItems>
+                <UserButton.Link
+                label="Dashboard"
+                labelIcon={<ChartColumn size={15}/>}
+                href="/dashboard"
+                />
+                </UserButton.MenuItems>
+            </UserButton>
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
